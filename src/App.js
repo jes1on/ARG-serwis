ad5dda9a3f21039ca09e4d2f0cd06e31afdf0b08
Szymon <szymonl97@wp.pl>
2023-01-18 19:39:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { Component }  from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      YO
+      YO
+      YO
+    </div>
+  );
+}
+
+export default App;
