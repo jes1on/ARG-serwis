58e05eeed22c4d12dd9fb6c775815ff1d11800fb
Szymon <szymonl97@wp.pl>
2023-01-20 23:43:57 +0100

    Remove Footer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@ import './App.css';
 import React, { Component }  from 'react';
 import Navbar from './components/Navbar.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer.js';
 
 function App() {
   return (
     <>  
       <Router>
         <Navbar />
-          <Routes>
-            <Route path='/' exact />
-          </Routes>
-        <Footer />
           <Routes>
             <Route path='/' exact />
           </Routes>
