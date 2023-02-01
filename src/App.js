8eaedf3e9f16f8a5ab06aa3b7a0a48f838184ab5
Szymon <szymonl97@wp.pl>
2023-02-01 21:00:08 +0100

    Developed BB suggestions connected with Footerjscs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { Component }  from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer.js';
+import {Footer} from './components/Footer.js';
 
 function App() {
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
 
-function Footer() {
+export const Footer = () => {
   return (
     <>
         <footer className='footer'>
-            <div className='footer-container'>   
-                <div className='footer-loc'>
+            <div className='footerContainer'>   
+                <div className='footerLoc'>
                     <h2>Lokalizacja</h2>
                         ul. G. Ziętka x/D<br></br>
                         40-400 Katowice
                 </div>
-                <div className='footer-menu'>
-                    <Link to="/" className='footer-link'>
+                <div className='footerMenu'>
+                    <Link to="/" className='footerLink'>
                         OFERTA
                     </Link>
-                    <Link to="/" className='footer-link'>
+                    <Link to="/" className='footerLink'>
                         GEOMETRIA 3D
                     </Link>
-                    <Link to="/" className='footer-link'>
+                    <Link to="/" className='footerLink'>
                         KLASYKI
                     </Link>
-                    <Link to="/" className='footer-link'>
+                    <Link to="/" className='footerLink'>
                         CENNIK
                     </Link>
-                    <Link to="/" className='footer-link'>
+                    <Link to="/" className='footerLink'>
                         GALERIA
                     </Link>
-                    <Link to="/" className='footer-link'>
+                    <Link to="/" className='footerLink'>
                         KONTAKT
                     </Link>
                 </div>
-                <div className='footer-call-container'>
+                <div className='footerCallContainer'>
                         <h1>Zadzwoń do nas</h1> 
                     
                         123 123 123
@@ -45,5 +45,3 @@ function Footer() {
     </>
   );
 }
-
-export default Footer;
