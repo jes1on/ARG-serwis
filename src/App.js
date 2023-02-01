aa0e59428adb4d96237665b9daf79316f9880901
Szymon <szymonl97@wp.pl>
2023-02-01 21:04:55 +0100

    Developed BB sugestions 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Component }  from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Footer} from './components/Footer.js';
 
