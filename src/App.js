4268120c0910636f5c7c7fac9e953daba57f8726
Szymon <szymonl97@wp.pl>
2023-02-02 23:14:36 +0100

    ???

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Home} from './components/Home.js';
 import {Footer} from './components/Footer.js';
 
 function App() {
   return (
     <>  
       <Router>
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
+        <Home />
+          <Routes>
+            <Route path='/' exact component={Home} />
+          </Routes>
         <Footer />
       </Router>
     </>
