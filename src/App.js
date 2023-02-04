ecdbb42361268a66b8ebabe090daa6236b4aea69
Szymon <szymonl97@wp.pl>
2023-02-04 16:51:45 +0100

    Footer changed <>

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Footer} from './components/Footer.js';
 
-export const App = () => (
-    <>  
-      <Router>
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
-  );
+export const App = () => (  
+  <Router>
+    <Routes>
+      <Route path='/' exact />
+    </Routes>
+    <Footer />
+  </Router>
+);
