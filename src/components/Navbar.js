40135076a47d347f58341c34fe903dd077e3a2d1
Szymon <101150231+jes1on@users.noreply.github.com>
2023-09-21 00:06:40 +0200

    Ok lets go

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,94 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/fontawesome-svg-core';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faxmark } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [navbar, setNavbar] = useState(false);
+  const [click, setClick] = useState(false);
+  const [navbar, setNavbar] = useState(false);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const changeBackground = () => {
-        if (window.scrollY >= 80) {
-            setNavbar(true);
-        } else {
-            setNavbar(false);
-        }
-    };
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
 
-    window.addEventListener('scroll', changeBackground);
+  window.addEventListener("scroll", changeBackground);
 
   return (
     <>
-        <nav className={navbar ? 'navbar active' : 'navbar'}>
-            <div className='navbar-container'>      
-                <Link to="/" className='navbar-logo' onClick={handleClick}>
-                    ARGSerwis
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to="/" className='nav-links' onClick={closeMobileMenu}>
-                            OFERTA
-                        </Link>
-                    </li>
-                    <li className='nav-item'>                    
-                        <Link to="/" className='nav-links' onClick={closeMobileMenu}>
-                            GEOMETRIA 3D
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to="/" className='nav-links' onClick={closeMobileMenu}>
-                            KLASYKI
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to="/" className='nav-links' onClick={closeMobileMenu}>
-                            CENNIK
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to="/" className='nav-links' onClick={closeMobileMenu}>
-                            GALERIA
-                        </Link>
-                    </li>                      
-                </ul>
-            </div>
-        </nav>
-        <div className='lolo'>
-                lolo
-            </div>
+      <nav className={navbar ? "navbar active" : "navbar"}>
+        <div className="navbar-container">
+          <Link to="/home" className="navbar-logo" onClick={handleClick}>
+            ARGSerwis
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i
+              className={
+                click ? (
+                  <FontAwesomeIcon icon={faxmark} />
+                ) : (
+                  <FontAwesomeIcon icon={faBars} />
+                )
+              }
+            />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link
+                to="/oferta"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                OFERTA
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/geometria-3d"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                GEOMETRIA 3D
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/klasyki"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                KLASYKI
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/cennik"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                CENNIK
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/galeria"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                GALERIA
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 }
